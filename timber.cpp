7dfd198e1b5b67bf5701bb232c0a0c43f34fa785
Anthony Le Goff <legoff.ant@gmail.com>
2022-06-06 14:49:05 +0200

    init window v2

--- timber.cpp
+++ timber.cpp
@@ -5,11 +5,25 @@
 
 int main() {
 	
+	
 	// Creer un objet "vm" mode video de 1920x1080
 	sf::VideoMode vm(1920, 1080);
 
 	// Creer et ouvrir une fenetre pour le jeu timber!
-	sf::RenderWindow windows(vm, "Timber!", sf::Style::Fullscreen);
+	sf::RenderWindow window(vm, "Timber!");
+
+	    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
 
 	return 0;
 }
